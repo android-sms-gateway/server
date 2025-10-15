4af39430176da5f49b87cf415bdde91773cdc2e4
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-10-15 20:12:29 +0700

    [events] use of pubsub instead of internal queue

--- internal/sms-gateway/modules/events/types.go
+++ internal/sms-gateway/modules/events/types.go
@@ -1,23 +1,33 @@
 package events
 
 import (
+	"encoding/json"
+
 	"github.com/android-sms-gateway/client-go/smsgateway"
 )
 
 type Event struct {
-	eventType smsgateway.PushEventType
-	data      map[string]string
+	EventType smsgateway.PushEventType `json:"event_type"`
+	Data      map[string]string        `json:"data"`
 }
 
-func NewEvent(eventType smsgateway.PushEventType, data map[string]string) *Event {
-	return &Event{
-		eventType: eventType,
-		data:      data,
+func NewEvent(eventType smsgateway.PushEventType, data map[string]string) Event {
+	return Event{
+		EventType: eventType,
+		Data:      data,
 	}
 }
 
 type eventWrapper struct {
-	UserID   string
-	DeviceID *string
-	Event    *Event
+	UserID   string  `json:"user_id"`
+	DeviceID *string `json:"device_id,omitempty"`
+	Event    Event   `json:"event"`
+}
+
+func (w *eventWrapper) serialize() ([]byte, error) {
+	return json.Marshal(w)
+}
+
+func (w *eventWrapper) deserialize(data []byte) error {
+	return json.Unmarshal(data, w)
 }
