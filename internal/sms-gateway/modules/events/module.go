23b7da5c0dd7ff10abd561b95ba1f1308666a847
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-10-15 20:12:29 +0700

    [events] use of pubsub instead of internal queue

--- internal/sms-gateway/modules/events/module.go
+++ internal/sms-gateway/modules/events/module.go
@@ -14,11 +14,18 @@ var Module = fx.Module(
 	}),
 	fx.Provide(newMetrics, fx.Private),
 	fx.Provide(NewService),
-	fx.Invoke(func(lc fx.Lifecycle, svc *Service) {
+	fx.Invoke(func(lc fx.Lifecycle, svc *Service, logger *zap.Logger, sh fx.Shutdowner) {
 		ctx, cancel := context.WithCancel(context.Background())
 		lc.Append(fx.Hook{
 			OnStart: func(_ context.Context) error {
-				go svc.Run(ctx)
+				go func() {
+					if err := svc.Run(ctx); err != nil {
+						logger.Error("Error running events service", zap.Error(err))
+						if err := sh.Shutdown(fx.ExitCode(1)); err != nil {
+							logger.Error("Failed to shutdown", zap.Error(err))
+						}
+					}
+				}()
 				return nil
 			},
 			OnStop: func(_ context.Context) error {
