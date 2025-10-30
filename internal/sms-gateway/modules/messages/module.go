15ebd6aa4de74f1536895346e0f10b529f7cf293
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-10-30 14:12:46 +0700

    [messages] refactor, remove background tasks

--- internal/sms-gateway/modules/messages/module.go
+++ internal/sms-gateway/modules/messages/module.go
@@ -2,35 +2,25 @@ package messages
 
 import (
 	cacheFactory "github.com/android-sms-gateway/server/internal/sms-gateway/cache"
-	"github.com/android-sms-gateway/server/internal/sms-gateway/modules/cleaner"
 	"github.com/capcom6/go-infra-fx/db"
+	"github.com/go-core-fx/logger"
 	"go.uber.org/fx"
-	"go.uber.org/zap"
 )
 
 func Module() fx.Option {
 	return fx.Module(
 		"messages",
-		fx.Decorate(func(log *zap.Logger) *zap.Logger {
-			return log.Named("messages")
-		}),
+		logger.WithNamedLogger("messages"),
 		fx.Provide(func(factory cacheFactory.Factory) (cacheFactory.Cache, error) {
 			return factory.New("messages")
 		}, fx.Private),
 
 		fx.Provide(newMetrics, fx.Private),
-		fx.Provide(newRepository, fx.Private),
-		fx.Provide(newHashingTask, fx.Private),
+		fx.Provide(NewRepository, fx.Private),
+		fx.Provide(newHashingWorker, fx.Private),
 		fx.Provide(newCache, fx.Private),
 
 		fx.Provide(NewService),
-		fx.Provide(
-			cleaner.AsCleanable(
-				func(svc *Service) cleaner.Cleanable {
-					return svc
-				},
-			),
-		),
 	)
 }
 
