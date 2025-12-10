5ef1ede83364ae2479a5cc23a74a52b8af979cf4
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-12-11 06:23:58 +0700

    [auth] use separate OTP module

--- internal/sms-gateway/modules/auth/module.go
+++ internal/sms-gateway/modules/auth/module.go
@@ -1,8 +1,6 @@
 package auth
 
 import (
-	"context"
-
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 )
@@ -14,18 +12,5 @@ func Module() fx.Option {
 			return log.Named("auth")
 		}),
 		fx.Provide(New),
-		fx.Invoke(func(lc fx.Lifecycle, svc *Service) {
-			ctx, cancel := context.WithCancel(context.Background())
-			lc.Append(fx.Hook{
-				OnStart: func(_ context.Context) error {
-					go svc.Run(ctx)
-					return nil
-				},
-				OnStop: func(_ context.Context) error {
-					cancel()
-					return nil
-				},
-			})
-		}),
 	)
 }
--- internal/sms-gateway/modules/auth/service.go
+++ internal/sms-gateway/modules/auth/service.go
@@ -2,7 +2,6 @@ package auth
 
 import (
 	"context"
-	"crypto/rand"
 	"crypto/subtle"
 	"fmt"
 	"time"
@@ -10,8 +9,8 @@ import (
 	"github.com/android-sms-gateway/server/internal/sms-gateway/models"
 	"github.com/android-sms-gateway/server/internal/sms-gateway/modules/devices"
 	"github.com/android-sms-gateway/server/internal/sms-gateway/online"
+	"github.com/android-sms-gateway/server/internal/sms-gateway/otp"
 	"github.com/android-sms-gateway/server/internal/sms-gateway/users"
-	"github.com/capcom6/go-helpers/cache"
 	"go.uber.org/zap"
 )
 
@@ -24,17 +23,17 @@ type Service struct {
 	config Config
 
 	usersSvc   *users.Service
+	otpSvc     *otp.Service
 	devicesSvc *devices.Service
 	onlineSvc  online.Service
 
 	logger *zap.Logger
-
-	codesCache *cache.Cache[string]
 }
 
 func New(
 	config Config,
 	usersSvc *users.Service,
+	otpSvc *otp.Service,
 	devicesSvc *devices.Service,
 	onlineSvc online.Service,
 	logger *zap.Logger,
@@ -42,44 +41,24 @@ func New(
 	return &Service{
 		config: config,
 
-		usersSvc:   usersSvc,
+		usersSvc: usersSvc,
+
+		otpSvc:     otpSvc,
 		devicesSvc: devicesSvc,
 		onlineSvc:  onlineSvc,
 
 		logger: logger,
-
-		codesCache: cache.New[string](cache.Config{TTL: codeTTL}),
 	}
 }
 
 // GenerateUserCode generates a unique one-time user authorization code.
-func (s *Service) GenerateUserCode(userID string) (OneTimeCode, error) {
-	var code string
-	var err error
-
-	const bytesLen = 3
-	const maxCode = 1000000
-	b := make([]byte, bytesLen)
-	validUntil := time.Now().Add(codeTTL)
-	for range 3 {
-		if _, err = rand.Read(b); err != nil {
-			continue
-		}
-		num := (int(b[0]) << 16) | (int(b[1]) << 8) | int(b[2]) //nolint:mnd //bitshift
-		code = fmt.Sprintf("%06d", num%maxCode)
-
-		if err = s.codesCache.SetOrFail(code, userID, cache.WithValidUntil(validUntil)); err != nil {
-			continue
-		}
-
-		break
-	}
-
+func (s *Service) GenerateUserCode(ctx context.Context, userID string) (*otp.Code, error) {
+	code, err := s.otpSvc.Generate(ctx, userID)
 	if err != nil {
-		return OneTimeCode{}, fmt.Errorf("failed to generate code: %w", err)
+		return nil, fmt.Errorf("failed to generate code: %w", err)
 	}
 
-	return OneTimeCode{Code: code, ValidUntil: validUntil}, nil
+	return code, nil
 }
 
 func (s *Service) RegisterDevice(user users.User, name, pushToken *string) (*models.Device, error) {
@@ -130,10 +109,10 @@ func (s *Service) AuthorizeDevice(token string) (models.Device, error) {
 }
 
 // AuthorizeUserByCode authorizes a user by one-time code.
-func (s *Service) AuthorizeUserByCode(code string) (*users.User, error) {
-	userID, err := s.codesCache.GetAndDelete(code)
+func (s *Service) AuthorizeUserByCode(ctx context.Context, code string) (*users.User, error) {
+	userID, err := s.otpSvc.Validate(ctx, code)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get user by code: %w", err)
+		return nil, fmt.Errorf("failed to validate code: %w", err)
 	}
 
 	user, err := s.usersSvc.GetByUsername(userID)
@@ -142,24 +121,4 @@ func (s *Service) AuthorizeUserByCode(code string) (*users.User, error) {
 	}
 
 	return user, nil
-}
-
-// Run starts a ticker that triggers the clean function every hour.
-// It runs indefinitely until the provided context is canceled.
-func (s *Service) Run(ctx context.Context) {
-	ticker := time.NewTicker(1 * time.Hour)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-ticker.C:
-			s.clean(ctx)
-		}
-	}
-}
-
-func (s *Service) clean(_ context.Context) {
-	s.codesCache.Cleanup()
 }
