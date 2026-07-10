efb7b4833b040516119822155d1c50629508ef5a
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-07-10 11:02:32 +0700

    [api] use ListMessagesOptions from client-go

--- internal/sms-gateway/handlers/messages/params.go
+++ internal/sms-gateway/handlers/messages/params.go
@@ -1,9 +1,7 @@
 package messages
 
 import (
-	"errors"
-	"time"
-
+	"github.com/android-sms-gateway/client-go/smsgateway"
 	"github.com/android-sms-gateway/server/internal/sms-gateway/modules/messages"
 )
 
@@ -12,45 +10,25 @@ type thirdPartyPostQueryParams struct {
 	DeviceActiveWithin  int  `query:"deviceActiveWithin"  validate:"omitempty,min=1"`
 }
 
-type thirdPartyGetQueryParams struct {
-	StartDate      string `query:"from"           validate:"omitempty,datetime=2006-01-02T15:04:05Z07:00"`
-	EndDate        string `query:"to"             validate:"omitempty,datetime=2006-01-02T15:04:05Z07:00"`
-	State          string `query:"state"          validate:"omitempty,oneof=Pending Cancelling Cancelled Processed Sent Delivered Failed"`
-	DeviceID       string `query:"deviceId"       validate:"omitempty,len=21"`
-	Limit          int    `query:"limit"          validate:"omitempty,min=1,max=100"`
-	Offset         int    `query:"offset"         validate:"omitempty,min=0"`
-	IncludeContent bool   `query:"includeContent"`
-}
-
-func (p *thirdPartyGetQueryParams) Validate() error {
-	if p.StartDate != "" && p.EndDate != "" && p.StartDate > p.EndDate {
-		return errors.New("`from` date must be before `to` date") //nolint:err113 // won't be used directly
-	}
-
-	return nil
-}
+type thirdPartyGetQueryParams smsgateway.ListMessagesOptions
 
 func (p *thirdPartyGetQueryParams) ToFilter() messages.SelectFilter {
 	var filter messages.SelectFilter
 
-	if p.StartDate != "" {
-		if t, err := time.Parse(time.RFC3339, p.StartDate); err == nil {
-			filter.StartDate = t
-		}
+	if p.From != nil {
+		filter.StartDate = *p.From
 	}
 
-	if p.EndDate != "" {
-		if t, err := time.Parse(time.RFC3339, p.EndDate); err == nil {
-			filter.EndDate = t
-		}
+	if p.To != nil {
+		filter.EndDate = *p.To
 	}
 
-	if p.State != "" {
-		filter.State = append(filter.State, messages.ProcessingState(p.State))
+	if p.State != nil {
+		filter.State = append(filter.State, messages.ProcessingState(*p.State))
 	}
 
-	if p.DeviceID != "" {
-		filter.DeviceID = p.DeviceID
+	if p.DeviceID != nil {
+		filter.DeviceID = *p.DeviceID
 	}
 
 	return filter
@@ -62,16 +40,19 @@ func (p *thirdPartyGetQueryParams) ToOptions() messages.SelectOptions {
 	var options messages.SelectOptions
 	options.WithRecipients = true
 	options.WithStates = true
-	options.WithContent = p.IncludeContent
 
-	if p.Limit > 0 {
-		options.Limit = min(p.Limit, maxLimit)
+	if p.IncludeContent != nil {
+		options.WithContent = *p.IncludeContent
+	}
+
+	if p.Limit != nil {
+		options.Limit = min(*p.Limit, maxLimit)
 	} else {
 		options.Limit = 50
 	}
 
-	if p.Offset > 0 {
-		options.Offset = p.Offset
+	if p.Offset != nil {
+		options.Offset = max(*p.Offset, 0)
 	}
 
 	return options
