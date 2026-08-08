4c73a66185d9bd028f2c948d314781f8a78947ff
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-08-08 17:56:01 +0700

    [db] squash migrations

--- internal/sms-gateway/models/migrations/mysql/20260728000000_add_device_e2e_keys.sql
+++ internal/sms-gateway/models/migrations/mysql/20260728000000_add_device_e2e_keys.sql
@@ -7,8 +7,16 @@ ADD `public_key` text NULL;
 ALTER TABLE `devices`
 ADD `key_version` int NULL DEFAULT NULL;
 -- +goose StatementEnd
+-- +goose StatementBegin
+ALTER TABLE `message_recipients`
+MODIFY COLUMN `phone_number` varchar(512) NOT NULL;
+-- +goose StatementEnd
 ---
 -- +goose Down
+-- +goose StatementBegin
+ALTER TABLE `message_recipients`
+MODIFY COLUMN `phone_number` varchar(128) NOT NULL;
+-- +goose StatementEnd
 -- +goose StatementBegin
 ALTER TABLE `devices` DROP `key_version`;
 -- +goose StatementEnd
